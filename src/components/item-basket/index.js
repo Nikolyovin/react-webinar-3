8f7d3abf9304f12d3baba5ebf3a1db9cdea64a50
Лёвин Николай Сергеевич <151705@grpz.su>
2023-12-08 07:57:42 +0300

    fixed working navigate from basket

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -1,30 +1,41 @@
-import {memo, useCallback} from 'react';
-import propTypes from 'prop-types';
-import {numberFormat} from "../../utils";
-import {cn as bem} from "@bem-react/classname";
+import { memo, useCallback } from "react";
+import propTypes from "prop-types";
+import { numberFormat } from "../../utils";
+import { cn as bem } from "@bem-react/classname";
 import PropTypes from "prop-types";
-import './style.css';
+import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 function ItemBasket(props) {
+  const navigate = useNavigate();
 
-  const cn = bem('ItemBasket');
+  const cn = bem("ItemBasket");
 
   const callbacks = {
-    onRemove: (e) => props.onRemove(props.item._id)
+    onRemove: (e) => props.onRemove(props.item._id),
+    onClick: () => {
+      props.closeModal(props.item._id);
+      navigate(`/app/${props.item._id}`);
+      // console.log("props", props);
+    },
   };
 
   return (
     <div className={cn()}>
-      <div className={cn('title')}>{props.item.title}</div>
-      <div className={cn('right')}>
-        <div className={cn('cell')}>{numberFormat(props.item.price)} ₽</div>
-        <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>
-        <div className={cn('cell')}>
+      <div onClick={callbacks.onClick} className={cn("title")}>
+        {props.item.title}
+      </div>
+      <div className={cn("right")}>
+        <div className={cn("cell")}>{numberFormat(props.item.price)} ₽</div>
+        <div className={cn("cell")}>
+          {numberFormat(props.item.amount || 0)} шт
+        </div>
+        <div className={cn("cell")}>
           <button onClick={callbacks.onRemove}>Удалить</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 ItemBasket.propTypes = {
@@ -32,13 +43,13 @@ ItemBasket.propTypes = {
     _id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     title: PropTypes.string,
     price: PropTypes.number,
-    amount: PropTypes.number
+    amount: PropTypes.number,
   }).isRequired,
   onRemove: propTypes.func,
-}
+};
 
 ItemBasket.defaultProps = {
   onRemove: () => {},
-}
+};
 
 export default memo(ItemBasket);
